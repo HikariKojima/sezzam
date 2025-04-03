718d731128e2946e4e4fadda16f8da90ac8afbcf
DevTare3 <tarik.mujezinovicbiz@gmail.com>
2025-04-03 20:57:50 +0200

    initial

---
+++ src/lib/components/myComponents/header/headerComponents/searchBarMobile.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+  import searchIcon from "$lib/assets/Search.svg";
+  import closeIcon from "$lib/assets/close.svg";
+
+  let query = $state("");
+
+  function handleSearch() {
+    if (query.trim()) {
+      //
+    }
+  }
+</script>
+
+<div
+  class=" flex items-center content-center gap-2.5 rounded-4xl my-2 py-2 px-4 max-w-fit mx-auto outline-2 outline-[#7B7B7B]"
+>
+  <div class="flex items-center content-center flex-row w-xs gap-2.5 max-w-fit">
+    <input
+      class="border-none px-4 py-2 outline-none"
+      type="search"
+      bind:value={query}
+      placeholder="Pretrazite..."
+    />
+    <img
+      class="cursor-pointer"
+      src={searchIcon}
+      alt="icon of a magnifying glass"
+    />
+    <img class="cursor-pointer hidden" src={closeIcon} alt="icon of a cross" />
+  </div>
+</div>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,5 @@
+<script lang="ts">
+  import SearchMobile from "$lib/components/myComponents/header/headerComponents/searchBarMobile.svelte";
+</script>
+
+<SearchMobile></SearchMobile>
