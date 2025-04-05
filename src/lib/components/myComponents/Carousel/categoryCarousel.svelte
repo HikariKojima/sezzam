32a322d8601432f3831745c674556c794a822a5f
DevTare3 <tarik.mujezinovicbiz@gmail.com>
2025-04-05 19:49:48 +0200

    added category carousel

---
+++ src/lib/components/myComponents/Carousel/categoryCarousel.svelte
@@ -0,0 +1,42 @@
+<script lang="ts">
+  import * as Carousel from "$lib/components/ui/carousel/index.js";
+  import * as Card from "$lib/components/ui/card/index.js";
+
+  export let categories: Array<{
+    id: number;
+    img: string;
+    name: string;
+  }>;
+</script>
+
+<h2 class="font-bold text-2xl text-center mb-4 mt-4">Pogledajte Kategorije</h2>
+<Carousel.Root
+  class=" w-full max-w-sm md:max-w-3xl mx-auto "
+  opts={{
+    loop: true,
+    align: "start",
+  }}
+>
+  <Carousel.Content class="-ml-4">
+    {#each categories as category (category.id)}
+      <Carousel.Item
+        class="pl-4 basis-full sm-basis-1/2 md:basis-1/3 lg:basis-1/4"
+      >
+        <Card.Root class="h-full flex flex-col">
+          <Card.Header class="flex-1">
+            <div class="aspect-video overflow-hidden bg-black">
+              <img
+                class="w-full h-full object-cover"
+                src={category.img}
+                alt={category.name}
+              />
+            </div>
+            <Card.Title class="mt-4 line-clamp-2">{category.name}</Card.Title>
+          </Card.Header>
+        </Card.Root>
+      </Carousel.Item>
+    {/each}
+  </Carousel.Content>
+  <Carousel.Previous />
+  <Carousel.Next />
+</Carousel.Root>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,29 @@
 <script lang="ts">
   import HeaderMobile from "$lib/components/myComponents/header/headerMobile.svelte";
-  import ActionCarousel from "$lib/components/myComponents/ActionProducts/productsOnAction.svelte";
-
+  import ActionCarousel from "$lib/components/myComponents/Carousel/productsOnAction.svelte";
+  import CategoryCarousel from "$lib/components/myComponents/Carousel/categoryCarousel.svelte";
+  import Tile from "$lib/assets/Tiles.svg";
+  import Floor from "$lib/assets/WoodenFloor.svg";
   export let data;
+
+  const categories = [
+    {
+      id: 1,
+      img: Tile,
+      name: "Zidne Plocice",
+    },
+
+    {
+      id: 2,
+      img: Tile,
+      name: "Podne Plocice",
+    },
+    {
+      id: 3,
+      img: Floor,
+      name: "Podovi",
+    },
+  ];
 </script>
 
 <HeaderMobile></HeaderMobile>
@@ -15,4 +36,5 @@
   <div class="mx-auto ml-12">
     <ActionCarousel products={data.products}></ActionCarousel>
   </div>
+  <CategoryCarousel {categories}></CategoryCarousel>
 </main>
