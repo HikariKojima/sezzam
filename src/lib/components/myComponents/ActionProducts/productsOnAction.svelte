3a64d85c0ea1d3f6465940529bf9dc07806f6b83
DevTare3 <tarik.mujezinovicbiz@gmail.com>
2025-04-05 17:32:54 +0200

    added carousel

--- src/lib/components/myComponents/ActionProducts/productsOnAction.svelte
+++ src/lib/components/myComponents/ActionProducts/productsOnAction.svelte
@@ -12,28 +12,36 @@
   }>;
 </script>
 
-<p>Pogledajte Kategorije</p>
+<h2 class="font-bold text-2xl text-center mb-4">Aktuelne Akcije</h2>
 <Carousel.Root
-  class="w-full max-w-md"
+  class=" w-full max-w-sm md:max-w-3xl mx-auto "
   opts={{
     loop: true,
     align: "start",
   }}
 >
-  <Carousel.Content class="-ml-1">
+  <Carousel.Content class="-ml-4">
     {#each products as product (product.id)}
-      <Carousel.Item class="md:basis-1/2 pl-1 md:pl-4 basis-1/3">
-        <Card.Root>
-          <Card.Header>
-            <img src={product.thumbnail} alt={product.description} />
-            <Card.Title class="mb-4">{product.title}</Card.Title>
+      <Carousel.Item
+        class="pl-4 basis-full sm-basis-1/2 md:basis-1/3 lg:basis-1/4"
+      >
+        <Card.Root class="h-full flex flex-col">
+          <Card.Header class="flex-1">
+            <div class="aspect-video overflow-hidden">
+              <img
+                class="w-full h-full object-cover"
+                src={product.thumbnail}
+                alt={product.description}
+              />
+            </div>
+            <Card.Title class="mt-4 line-clamp-2">{product.title}</Card.Title>
             <Card.Description>{product.title}</Card.Description>
           </Card.Header>
-          <Card.Content
-            class="flex aspect-square items-center justify-center p-6"
-          >
-            <div class="flex items-center content-center flex-col">
-              <p class="text-red-500 font-poppins">{product.price}km/m2</p>
+          <Card.Content class="mt-auto p-4">
+            <div class="flex items-center content-center flex-col p-1">
+              <p class="text-red-500 font-bold text-2xl">
+                {product.price}km/m2
+              </p>
               <p class="line-through">{product.price}km/m2</p>
             </div>
           </Card.Content>
