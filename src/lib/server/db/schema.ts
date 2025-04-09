2c510dba0cf9eb48affb4dbaad493457e2dcc402
DevTare3 <tarik.mujezinovicbiz@gmail.com>
2025-04-09 20:00:12 +0200

    added initial db

--- src/lib/server/db/schema.ts
+++ src/lib/server/db/schema.ts
@@ -1,18 +1,58 @@
-import { pgTable, serial, text, integer, timestamp } from 'drizzle-orm/pg-core';
+import {
+  pgTable,
+  serial,
+  text,
+  integer,
+  timestamp,
+  boolean,
+  decimal,
+} from "drizzle-orm/pg-core";
 
-export const user = pgTable('user', {
-	id: text('id').primaryKey(),
-	age: integer('age'),
-	username: text('username').notNull().unique(),
-	passwordHash: text('password_hash').notNull()
+export const user = pgTable("user", {
+  id: text("id").primaryKey(),
+  age: integer("age"),
+  username: text("username").notNull().unique(),
+  email: text("email").notNull().unique(),
+  passwordHash: text("password_hash").notNull(),
 });
 
-export const session = pgTable('session', {
-	id: text('id').primaryKey(),
-	userId: text('user_id').notNull().references(() => user.id),
-	expiresAt: timestamp('expires_at', { withTimezone: true, mode: 'date' }).notNull()
+export const categories = pgTable("categories", {
+  id: integer("id").primaryKey(),
+  name: text("name").notNull(),
+  image: text("image").notNull(),
+});
+
+export const session = pgTable("session", {
+  id: text("id").primaryKey(),
+  userID: text("user_id")
+    .notNull()
+    .references(() => user.id),
+  expiresAt: timestamp("expiresAt", {
+    withTimezone: true,
+    mode: "date",
+  }).notNull(),
+});
+
+export const products = pgTable("products", {
+  id: integer("id").primaryKey(),
+  name: text("name").notNull(),
+  description: text("description").notNull(),
+  price: decimal("price", { precision: 12, scale: 2 }).notNull(),
+  imageID: integer("image_id").notNull(),
+  categoryID: integer("category_id")
+    .notNull()
+    .references(() => categories.id),
+  featured: boolean("featured").notNull(),
+  stock: integer("stock").notNull(),
+  created_at: timestamp("created_at", {
+    withTimezone: true,
+    mode: "date",
+  })
+    .notNull()
+    .defaultNow(),
 });
 
 export type Session = typeof session.$inferSelect;
-
+export type Product = typeof products.$inferSelect;
+export type Categories = typeof categories.$inferSelect;
 export type User = typeof user.$inferSelect;
