5b489d9c87ad7a896e86d530b2e6ba74be486d10
DevTare3 <tarik.mujezinovicbiz@gmail.com>
2025-04-24 19:54:57 +0200

    added user cart api

--- src/routes/api/cart/+server.ts
+++ src/routes/api/cart/+server.ts
@@ -5,15 +5,27 @@ import { and, eq, sql } from "drizzle-orm";
 
 export async function GET({ url }: RequestEvent) {
   try {
-    const cartID = url.searchParams.get("id");
-    if (!cartID) {
+    const userId = url.searchParams.get("userId");
+    if (!userId) {
       return json({ error: "Cart ID is required" }, { status: 400 });
     }
-    const cartItems = await db
+    const userCart = await db
       .select()
       .from(cart)
-      .where(and(eq(cart.userId, cartID), eq(cart.status, "active")));
-    return json(cartItems);
+      .where(and(eq(cart.userId, userId), eq(cart.status, "active")));
+
+    if (!userCart.length) {
+      return json({ items: [] });
+    }
+
+    const cartItems = await db
+      .select()
+      .from(cartProducts)
+      .where(eq(cartProducts.cartId, userCart[0].id));
+    return json({
+      cart: userCart[0],
+      items: cartItems,
+    });
   } catch (error) {
     console.error("Error fetching cart items:", error);
     return json({ error: "Failed to fetch cart items" }, { status: 500 });
@@ -70,5 +82,41 @@ export async function POST({ request }: RequestEvent) {
   } catch (error) {
     console.error("Error adding product to cart:", error);
     return json({ error: "Failed to add product to cart" }, { status: 500 });
+  }
+}
+
+export async function DELETE({ url }: RequestEvent) {
+  try {
+    const userId = url.searchParams.get("userId");
+    if (!userId) {
+      return json({ error: "User ID is required" }, { status: 400 });
+    }
+
+    const userCart = await db
+      .select()
+      .from(cart)
+      .where(and(eq(cart.userId, userId), eq(cart.status, "active")));
+    if (!userCart.length) {
+      return json(
+        { error: "No active cart found for this user" },
+        { status: 404 }
+      );
+    }
+
+    await db
+      .delete(cartProducts)
+      .where(eq(cartProducts.cartId, userCart[0].id));
+
+    await db
+      .update(cart)
+      .set({
+        totalAmount: "0.00",
+        status: "cleared",
+      })
+      .where(eq(cart.id, userCart[0].id));
+    return json({ message: "Cart cleared successfully" });
+  } catch (error) {
+    console.error("Error clearing cart:", error);
+    return json({ error: "Failed to clear cart" }, { status: 500 });
   }
 }
