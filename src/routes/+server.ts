d50f06eb6f4f0bb7e43e68e7345168e887c9f409
DevTare3 <tarik.mujezinovicbiz@gmail.com>
2025-05-06 19:40:22 +0200

    started working on the project again

--- src/routes/+server.ts
+++
@@ -1,9 +0,0 @@
-import type { RequestEvent } from "./$types";
-
-// export function GET(event: RequestEvent): Promise<Response> {
-//   if (event.locals.user === null) {
-//     return new Response(null, {
-//       status: 401,
-//     });
-//   }
-// }
---
+++ src/routes/[productID]/+page.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+  import { Plus, Minus, ShoppingCart } from "@lucide/svelte";
+  import { DateRangeField } from "bits-ui";
+  let { data } = $props();
+  console.log(data);
+
+  let quantity = 1;
+
+  function updateQuantity(change: number) {
+    quantity = Math.max(1, quantity + change);
+  }
+
+  //   async function addToCart() {
+  //     try {
+  //         const response = await fetch('/api/cart' , {
+  //             method: 'POST',
+  //             headers: {
+  //                 'Content-Type': 'application/json'
+  //             },
+  //             body: JSON.stringify({
+  //                 productId: data,
+  //                 quantity,
+  //                 price: data.price
+  //             })
+  //         })
+  //     }
+  //   }
+</script>
