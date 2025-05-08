07c84b47f6c4f6377e6108d300bd4e8bff1df2e6
DevTare3 <tarik.mujezinovicbiz@gmail.com>
2025-05-08 16:08:58 +0200

    Revert "started working on the project again"
    This reverts commit d50f06eb6f4f0bb7e43e68e7345168e887c9f409.

---
+++ src/routes/+server.ts
@@ -0,0 +1,9 @@
+import type { RequestEvent } from "./$types";
+
+// export function GET(event: RequestEvent): Promise<Response> {
+//   if (event.locals.user === null) {
+//     return new Response(null, {
+//       status: 401,
+//     });
+//   }
+// }
